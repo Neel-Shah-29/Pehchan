e2641119d0eef09fbdbd5f4cb4b17c1ed6713caf
harshilshah99 <vharshilshah19052002@gmail.com>
2023-02-11 14:26:26 +0530

    frontend

---
+++ public/src/components/Body.js
@@ -0,0 +1,13 @@
+import Navb from "./Navb";
+import Home from "./Home";
+
+const Body = () => {
+    return ( 
+        <>
+        <Navb/>
+        <Home/>
+        </>
+     );
+}
+ 
+export default Body;
